42a78f2a826893574cd5511441d2fe9c9b8a1862
mapxion <dggfreelance@gmail.com>
2026-01-27 19:21:51 +0100

    bump version v2

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ pool.query("select 1")
   .catch(err => console.error("Error Postgres", err));
 
 app.get("/version", (req, res) => {
-  res.json({ version: "v1-initdb" });
+  res.json({ version: "v2-jobs-get" });
+
 });
 
 // ✅ INIT DB (temporal)
